6e7f14747d54cbca8c85dc8708d7a5a7baebe713
Maria Westling <Maria@n144-p41.kthopen.kth.se>
2016-03-21 15:15:24 +0100

    Fixed error in results

--- js/view/allDishesView.js
+++ js/view/allDishesView.js
@@ -16,6 +16,8 @@ var AllDishesView = function (container, model) {
 
 	// function that loads all the dishes
 	var allDishes = [];
+	var dishResults = null;
+	var errorMessage = document.createElement('div');
 	this.loadDishes = function(searchWord) {
 		this.currType = String(container.find("#dishType option:selected").val());
 		model.getAllDishes(this.currType, searchWord, function(dishes){
@@ -23,35 +25,54 @@ var AllDishesView = function (container, model) {
 			//console.log("in callback?");
 			//console.log(dishes);
 			var allDishDivs = document.createElement('div');
-			for (var i = 0; i < 4; i++) {
-				//console.log("in foor loop");
-				var currentDish = dishes.Results[i];
-				//console.log("for loop, dishname: ", currentDish.Title);
-
-
-				//skapar ett div-element
-				var dishDiv = document.createElement('div');
-				dishDiv.className = "dish-container"; //lägger till classname
-				dishDiv.id = String(currentDish.RecipeID); //lägger till id
-				//lägger till innehållet i dishdiv (info om dishen)
-				dishDiv.innerHTML = "<div class='dish-image'><img src='" 
-				+ String(currentDish.ImageURL) + "'/></div><div class='dish-name'>" 
-				+ String(currentDish.Title) 
-				+ "</div><div class='dish-info'>" + String(currentDish.Subcategory) 
-				+ "</div>";
+			if (dishes.ResultCount === 0) {
+				dishResults = false;
 				
-				//lägger in dishDiven i containern
-				//this.dishContainer.append(dishDiv);
-				allDishes.push(dishDiv);
-				//console.log("This is our allDishes",allDishes);
+				errorMessage.innerHTML = "We could not find what you were searching for. Please try again.";
+			}
+			
+			else {
+				dishResults = true;
+				for (var i = 0; i < 4; i++) {
+
+					//console.log("in foor loop");
+					var currentDish = dishes.Results[i];
+					//console.log("for loop, dishname: ", currentDish.Title);
+
+
+					//skapar ett div-element
+					var dishDiv = document.createElement('div');
+					
+
+						dishDiv.className = "dish-container"; //lägger till classname
+						dishDiv.id = String(currentDish.RecipeID); //lägger till id
+						//lägger till innehållet i dishdiv (info om dishen)
+						dishDiv.innerHTML = "<div class='dish-image'><img src='" 
+						+ String(currentDish.ImageURL) + "'/></div><div class='dish-name'>" 
+						+ String(currentDish.Title) 
+						+ "</div><div class='dish-info'>" + String(currentDish.Subcategory) 
+						+ "</div>";
+
+					
+					//lägger in dishDiven i containern
+					//this.dishContainer.append(dishDiv);
+					allDishes.push(dishDiv);
+					//console.log("This is our allDishes",allDishes);
+				}
 			}
 		});
 		//console.log("YO",allDishes);
-		for (var j = 0; j < allDishes.length; j++) {
-			//console.log("alldishes[j]",allDishes[j]);
-			this.dishContainer.append(allDishes[j]);
+		if (dishResults === false) {
+			this.dishContainer.append(errorMessage);
 		}
-		allDishes = [];
+		else {
+			for (var j = 0; j < allDishes.length; j++) {
+				//console.log("alldishes[j]",allDishes[j]);
+				this.dishContainer.append(allDishes[j]);
+			}
+
+			allDishes = [];
+		}
 		//console.log("this.dishContainer", this.dishContainer);
 		//console.log("loadDishes AllDishes call: ");
 
