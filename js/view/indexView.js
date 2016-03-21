abeac23a345a00d9ce839726b4a669041f346775
Riverbell <emba93@live.se>
2016-03-21 14:26:18 +0100

    Cleaned up some code

--- js/view/indexView.js
+++ js/view/indexView.js
@@ -3,6 +3,5 @@ var IndexView = function(container, model) {
 
 	this.container = container;
 	this.continueButton = container.find("#continueToDinner");
-	console.log("contbutton", this.continueButton);
 
 }
